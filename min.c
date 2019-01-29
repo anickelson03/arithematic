06b41692c56fd552b73ec0c960b45bb5e1ccf2e0
anickelson03 <46019633+anickelson03@users.noreply.github.com>
2019-01-29 11:43:20 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+{
+int a,b,lcm,gcd;
+scanf("%d %d",&a,&b);
+for(int i=2;i<=a&&i<=b;i++)
+{
+if(a%i==0 && b%i==0)
+{
+gcd=i;
+}
+}
+lcm=(a*b)/gcd;
+printf("%d",lcm);
+return 0;
+}
